eeb732d01a51b5264d7b68cf456de918423c7708
stepansubbotin <stepan.v.subbotin@gmail.com>
2015-01-13 15:48:06 -0600

    Added to teleopPeriodic command

--- src/org/usfirst/frc/team4536/robot/Robot.java
+++ src/org/usfirst/frc/team4536/robot/Robot.java
@@ -7,23 +7,25 @@ import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends IterativeRobot {
 	DriveTrain driveTrain;
-	Joystick leftStick;
-	Joystick rightStick;
-	
+	Joystick mainStick;
+	Joystick secondaryStick;
 	
     public void robotInit() {
     	driveTrain = new DriveTrain(Constants.LEFT_TALON_CHANNEL, Constants.RIGHT_TALON_CHANNEL);
-    	leftStick = new Joystick(Constants.LEFT_STICK_PORT);
-    	rightStick = new Joystick(Constants.RIGHT_STICK_PORT);
+        mainStick = new Joystick(Constants.LEFT_STICK_PORT);
+    	secondaryStick = new Joystick(Constants.RIGHT_STICK_PORT);
     }
 
     public void autonomousPeriodic() {
-
+  
     }
 
 
     public void teleopPeriodic() {
-        
+    	double forwardThrottle = mainStick.getY();
+    	double turnThrottle = mainStick.getX();
+    	
+    	driveTrain.drive(forwardThrottle, turnThrottle);
     }
     
 
