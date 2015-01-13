994502d6f02928074082c983ef2e80be76ea545f
stepansubbotin <stepan.v.subbotin@gmail.com>
2015-01-12 23:26:29 -0600

    Added Base Project
    Added the eclipse iterative frc project

---
+++ src/org/usfirst/frc/team4536/robot/Robot.java
@@ -0,0 +1,37 @@
+
+package org.usfirst.frc.team4536.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+
+public class Robot extends IterativeRobot {
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    
+    }
+    
+}
