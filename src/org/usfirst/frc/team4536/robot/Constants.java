3cea19b2dc66d1da48d5ebf7b2d80e5a3c5ac0c3
stepansubbotin <stepan.v.subbotin@gmail.com>
2015-01-13 15:04:33 -0600

    Added constants.java
    Added a constants class

---
+++ src/org/usfirst/frc/team4536/robot/Constants.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team4536.robot;
+
+public class Constants {
+	public static final int LEFT_STICK_PORT = 0;
+	public static final int RIGHT_STICK_PORT = 1;
+	public static final int LEFT_TALON_CHANNEL = 0;
+	public static final int RIGHT_TALON_CHANNEL = 1;
+}
