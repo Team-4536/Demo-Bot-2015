70c31897df9b2af05c5f0362160e28641039dcf5
stepansubbotin <stepan.v.subbotin@gmail.com>
2015-01-18 17:57:40 -0600

    Added comments to Constants class

--- src/org/usfirst/frc/team4536/robot/Constants.java
+++ src/org/usfirst/frc/team4536/robot/Constants.java
@@ -1,9 +1,15 @@
 package org.usfirst.frc.team4536.robot;
 
 public class Constants {
+	
+	//Joystick ports
 	public static final int LEFT_STICK_PORT = 0;
 	public static final int RIGHT_STICK_PORT = 1;
+	
+	//Talon ports
 	public static final int LEFT_TALON_CHANNEL = 0;
 	public static final int RIGHT_TALON_CHANNEL = 1;
+	
+	//Dead zone constant. Currently, a 12% dead zone on the joysticks.
 	public static final double DEAD_ZONE = 0.12;
 }
