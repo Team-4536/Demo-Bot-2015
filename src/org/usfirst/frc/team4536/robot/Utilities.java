c7bd852983d645e7438d2244034fea5f0431cde3
stepansubbotin <stepan.v.subbotin@gmail.com>
2015-01-18 17:52:47 -0600

    Added limit method to Utilities.java

--- src/org/usfirst/frc/team4536/robot/Utilities.java
+++ src/org/usfirst/frc/team4536/robot/Utilities.java
@@ -5,7 +5,7 @@ public class Utilities {
 	/*
 	 * Takes in values and makes sure they are both above deadZone and below -deadZone
 	 * We use it to create a small region on our joysticks that outputs 0
-	 * Once the joystick gets outside of the dead zone, it outputs the actual values of the joystick
+	 * Once the joystick gets outside of the dead zone, it outputs their actual values
 	 */
 	public static double deadZone (double input, double deadZone) {
         double output;
@@ -19,4 +19,22 @@ public class Utilities {
     }
 	
 	
+	/*
+	 * Takes in values and makes them between -1 and 1
+	 */
+	public static double limit (double input) {
+		double output;
+		
+		if(input > 1) {
+			output = 1;
+		}
+		else if(input < -1) {
+			output = -1;
+		}
+		else {
+			output = input;
+		}
+		
+		return output;
+	}
 }
