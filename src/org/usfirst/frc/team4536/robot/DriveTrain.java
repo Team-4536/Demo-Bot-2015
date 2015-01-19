f6897d2972fc704bd3715fa73e2462897d233685
Stepan Subbotin <stepan.v.subbotin@gmail.com>
2015-01-18 18:22:59 -0600

    Edited Formatting of DriveTrain.java

--- src/org/usfirst/frc/team4536/robot/DriveTrain.java
+++ src/org/usfirst/frc/team4536/robot/DriveTrain.java
@@ -8,33 +8,28 @@ public class DriveTrain {
 	Talon rightTalon;
 		
 	/*
-     * This function is the constructor for the DriveTrain class
-     * It takes in two arguments - the two PWM channels for the Talons.
-     */
+         * This function is the constructor for the DriveTrain class
+         * It takes in two arguments - the two PWM channels for the Talons.
+         */
 	public DriveTrain(int leftTalonChannel, int rightTalonChannel) {
 		leftTalon = new Talon(leftTalonChannel);
 		rightTalon = new Talon(rightTalonChannel);
 	}
 	
-	
 	/*
-     * This function is called in order to make the robot drive
-     * It takes in two arguments - the amount of forward throttle (-1 to 1)
-     * and the amount of turn throttle (-1 to 1). 
-     */
+     	 * This function is called in order to make the robot drive
+     	 * It takes in two arguments - the amount of forward throttle (-1 to 1)
+     	 * and the amount of turn throttle (-1 to 1). 
+     	 */
 	public void drive(double forwardThrottle, double turnThrottle) {
 		double leftTalonThrottle = forwardThrottle + turnThrottle;
 		double rightTalonThrottle = -forwardThrottle + turnThrottle;
 				
-		/*
-	     * Makes sure that the two Talon throttles are between -1 and 1
-	     */
+	     	// Makes sure that the two Talon throttles are between -1 and 1
 		Utilities.limit(leftTalonThrottle);
 		Utilities.limit(rightTalonThrottle);
 			
-		/*
-		 * These two lines set the values for the Talons based on the two throttle values
-		 */
+		// These two lines set the values for the Talons based on the two throttle values
 		leftTalon.set(leftTalonThrottle);
 		rightTalon.set(rightTalonThrottle);
 	}
