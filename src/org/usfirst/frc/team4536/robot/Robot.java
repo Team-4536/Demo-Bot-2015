4d423019a779228531d7f2fecb72e9c5d9714c48
AnonymousAnion <liam.tomsonmoylan@gmail.com>
2015-01-22 16:24:39 -0600

    I added basic compressor code. We are testing whether the compressor will run while disabled with this code.

--- src/org/usfirst/frc/team4536/robot/Robot.java
+++ src/org/usfirst/frc/team4536/robot/Robot.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4536.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -9,19 +10,23 @@ public class Robot extends IterativeRobot {
 	DriveTrain driveTrain;
 	Joystick mainStick;
 	Joystick secondaryStick;
+	Compressor compressor;
 	
 	public void robotInit() {
     	driveTrain = new DriveTrain(Constants.LEFT_TALON_CHANNEL, 
     					    		Constants.RIGHT_TALON_CHANNEL);
         mainStick = new Joystick(Constants.LEFT_STICK_PORT);
     	secondaryStick = new Joystick(Constants.RIGHT_STICK_PORT);
+    	compressor = new Compressor();
     }
 
 	public void autonomousPeriodic() {
-		
-    }
+		compressor.start();
+		}
 
 	public void teleopPeriodic() {
+		compressor.start();
+		
     	// Gets X and Y values from mainStick and puts a dead zone on them
       	double mainStickY = Utilities.deadZone(mainStick.getY(), Constants.DEAD_ZONE);
     	double mainStickX = Utilities.deadZone(mainStick.getX(), Constants.DEAD_ZONE);
@@ -37,7 +42,11 @@ public class Robot extends IterativeRobot {
     	driveTrain.drive(Utilities.deadZone(forwardThrottle, Constants.DEAD_ZONE), 
     				 	 Utilities.deadZone(turnThrottle, Constants.DEAD_ZONE));
     }
-    
+	
+	public void disabledPeriodic() {
+		
+	}
+
     public void testPeriodic() {
     		
     }
