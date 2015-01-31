8c63ae447b041fb4066c4eefe89327eb9112e714
MinuteBots <MinuteBots@10.11.51.241>
2015-01-31 09:46:55 -0600

    Fixed bug with driving and added limit switch code

--- src/org/usfirst/frc/team4536/robot/Robot.java
+++ src/org/usfirst/frc/team4536/robot/Robot.java
@@ -3,18 +3,21 @@ package org.usfirst.frc.team4536.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Robot extends IterativeRobot {
 	DriveTrain driveTrain;
 	Joystick mainStick;
 	Joystick secondaryStick;
+	DigitalInput limitSwitch1;
 	
 	public void robotInit() {
     	driveTrain = new DriveTrain(Constants.LEFT_TALON_CHANNEL, 
     					    		Constants.RIGHT_TALON_CHANNEL);
         mainStick = new Joystick(Constants.LEFT_STICK_PORT);
     	secondaryStick = new Joystick(Constants.RIGHT_STICK_PORT);
+    	
+    	limitSwitch1 = new DigitalInput(3);
     }
 
 	public void autonomousPeriodic() {
@@ -31,11 +34,12 @@ public class Robot extends IterativeRobot {
     	mainStickX = Utilities.speedCurve(mainStick.getX(), Constants.SPEED_CURVE);
     	
     	// Sets throttle values based on mainStick X and Y values (with dead zone).
-    	double forwardThrottle = mainStickX;
-    	double turnThrottle = mainStickY;
+    	double forwardThrottle = mainStickY;
+    	double turnThrottle = mainStickX;
     	
-    	driveTrain.drive(Utilities.deadZone(forwardThrottle, Constants.DEAD_ZONE), 
-    				 	 Utilities.deadZone(turnThrottle, Constants.DEAD_ZONE));
+    	driveTrain.drive(forwardThrottle, turnThrottle);
+    	
+    	SmartDashboard.putBoolean("Limit_Switch_Value", !limitSwitch1.get());
     }
     
     public void testPeriodic() {
