1a5c31f6394e1852ba85c4cd6baee6c900eb7daa
inkling16 <calebsyk@gmail.com>
2015-02-01 15:55:24 -0600

    Update Utilities.java

--- src/org/usfirst/frc/team4536/robot/Utilities.java
+++ src/org/usfirst/frc/team4536/robot/Utilities.java
@@ -6,6 +6,7 @@ public class Utilities {
 	
 	/*
 	 * Takes in values and makes them between -1 and 1
+	 *Suggested improvement: there is no need to have "output" in this method. Just use "return" in multiple places instead. Caleb
 	 */
 	public static double limit (double input) {
 		double output;
@@ -27,6 +28,7 @@ public class Utilities {
 	 * Takes in values and makes sure they are both above deadZone and below -deadZone
 	 * We use it to create a small region on our joysticks that outputs 0
 	 * Once the joystick gets outside of the dead zone, it outputs their actual values
+	 *Suggested improvement: Same as for limit(). Caleb
 	 */
 	public static double deadZone (double input, double deadZone) {
         double output;
@@ -41,7 +43,7 @@ public class Utilities {
 	
 	
 	/*
-	 * 
+	 * Suggested improvement: have comments right here. Caleb
 	 */
 	public static double speedCurve(double input, double curve) {
         double output;
