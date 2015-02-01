5e2ab354572991806d1b1fd8ee8db8b5219c7e6a
inkling16 <calebsyk@gmail.com>
2015-02-01 15:59:25 -0600

    Update Platform.java

--- src/org/usfirst/frc/team4536/robot/Platform.java
+++ src/org/usfirst/frc/team4536/robot/Platform.java
@@ -2,6 +2,10 @@ package org.usfirst.frc.team4536.robot;
 
 import edu.wpi.first.wpilibj.Solenoid;
 
+/*
+*Suggested improvement: There needs to be comments in this class! Caleb
+*/
+
 public class Platform {
 	
 	Solenoid rightSolenoid;
@@ -12,7 +16,9 @@ public class Platform {
 		rightSolenoid = new Solenoid(rightSolenoidChannel);
 		leftSolenoid = new Solenoid(leftSolenoidChannel);
 	}	
-	
+	/*
+	*Suggested improvement: change method name from "get" to something more intuitive, such as "isExtended" or "isRetracted." Caleb
+	*/
 	public boolean get() {
 		
 		return rightSolenoid.get(); // true = extended, false = retracted. This code works because the values must be opposite of each other and only come in 2 combinations.x
