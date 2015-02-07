1ed6a7dfd57af434eed0c522c850807d92c22981
MinuteBots <MinuteBots@MinuteBots-PC>
2015-02-07 11:35:42 -0600

    Added gyro code into the DriveTrain class

--- src/org/usfirst/frc/team4536/robot/DriveTrain.java
+++ src/org/usfirst/frc/team4536/robot/DriveTrain.java
@@ -2,10 +2,12 @@
 package org.usfirst.frc.team4536.robot;
 
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Gyro;
 
 public class DriveTrain {
 	Talon leftTalon;
 	Talon rightTalon;
+	Gyro gyroSensor;
 		
 	/*
      * This function is the constructor for the DriveTrain class
@@ -14,6 +16,20 @@ public class DriveTrain {
 	public DriveTrain(int leftTalonChannel, int rightTalonChannel) {
 		leftTalon = new Talon(leftTalonChannel);
 		rightTalon = new Talon(rightTalonChannel);
+		gyroSensor = new Gyro(Constants.GYRO_SENSOR_CHANNEL);
+	}
+	
+	public void startGyro() {
+		gyroSensor.initGyro();
+	}
+	
+	public void resetGyro() {
+		gyroSensor.reset();
+	}
+	
+	public double gyroGetAngle() {
+		double angle = gyroSensor.getAngle();
+		return angle;
 	}
 	
 	/*
@@ -32,6 +48,17 @@ public class DriveTrain {
 		// These two lines set the values for the Talons based on the two throttle values
 		leftTalon.set(leftTalonThrottle);
 		rightTalon.set(rightTalonThrottle);
+	}
+	
+	/*
+	 * This method can be used to make the drive train drive straight at a certain angle. 
+	 * It only works if the angle that is inputed is the current angle of the robot.
+	 */
+	public void driveStraight(double forwardThrottle, double angle) {
+		double angleDiff = angle - gyroSensor.getAngle();
+		double adjustment = angleDiff * Constants.PROPORTIONALITY_CONSTANT;
+		
+		this.drive(forwardThrottle, adjustment);				
 	}
 	
 }
