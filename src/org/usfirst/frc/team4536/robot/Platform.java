25ad3e3e49678357d05422b358eef0321a5af55f
AnonymousAnion <liam.tomsonmoylan@gmail.com>
2015-03-03 16:17:30 -0600

    Commented platform and tipper classes.

--- src/org/usfirst/frc/team4536/robot/Platform.java
+++ src/org/usfirst/frc/team4536/robot/Platform.java
@@ -7,8 +7,8 @@ public class Platform { //This a piston controlled platform to rest stacks on.
 	
 	Solenoid rightSolenoid;
 	Solenoid leftSolenoid;
-	Timer extendedTimer;
-	Timer retractedTimer;
+	Timer extendedTimer; // A timer for measuring the time that the platform has been extended.
+	Timer retractedTimer; // A timer for measuring the time that the platform has been retracted.
 	
 	public Platform(int rightSolenoidChannel, int leftSolenoidChannel) {	
 		rightSolenoid = new Solenoid(rightSolenoidChannel);
@@ -19,22 +19,22 @@ public class Platform { //This a piston controlled platform to rest stacks on.
 		extendedTimer.start();
 		retractedTimer.start();
 		
-		// Platform is initially retracted
-		this.retract();	//rightSolenoid is set true, leftSolenoid is set false.
+		// Platform is initialized as retracted
+		this.retract();	//rightSolenoid is initialized as true, leftSolenoid is initialized as false.
 	}	
 	
 	public boolean isExtended() {
 		
-		return rightSolenoid.get(); // true = extended, false = retracted. This code works because the values must be opposite of each other and only come in 2 combinations.x
+		return rightSolenoid.get(); // true = extended, false = retracted. This code works because the values must be opposite of each other and only come in 2 combinations.
 	}
 	
-	public void extend() {
+	public void extend() { // Sets the solenoid states to extend the platform.
 		
 		rightSolenoid.set(true);
 		leftSolenoid.set(false);
 	}
 	
-	public void retract() {
+	public void retract() { // Sets the solenoid states to retract the platform.
 		
 		rightSolenoid.set(false);
 		leftSolenoid.set(true);	
@@ -46,21 +46,21 @@ public class Platform { //This a piston controlled platform to rest stacks on.
 		leftSolenoid.set(!leftSolenoid.get());	
 	}
 
-	public double timeExtended() {
-		if (this.isExtended() == true) { // if is Extended
-			return extendedTimer.get();
+	public double timeExtended() { // Returns the time the platform has been extended.
+		if (this.isExtended() == true) { // if platform is extended.
+			return extendedTimer.get(); // Returns the time the platform has been extended.
 		} else {
-			extendedTimer.reset();
-			return 0;
+			extendedTimer.reset(); // Constantly resets the extendedTimer to zero each cycle of code when not extended. This is so the timer counts up from zero once it becomes extended.
+			return 0; // If not extended it returns a time of zero.
 		}
 	}
 	
-	public double timeRetracted() {
+	public double timeRetracted() { // Returns the time the platform has been retracted.
 		if (this.isExtended() == false) { // if is Retracted.
 			return retractedTimer.get();
 		} else {
-			retractedTimer.reset();
-			return 0;
+			retractedTimer.reset(); // Constantly resets the retractedTimer to zero each cycle of code when not retracted. This is so the timer counts up from zero once it becomes retracted.
+			return 0; //If not retracted it returns a time of zero.
 		}
 	}
 }
--- src/org/usfirst/frc/team4536/robot/Tipper.java
+++ src/org/usfirst/frc/team4536/robot/Tipper.java
@@ -2,12 +2,12 @@ package org.usfirst.frc.team4536.robot;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
 
-public class Tipper {
+public class Tipper { // This is a counter-weight for our robots stacks and also allows our robot to tip for picking up recycling containers.
 	
 	Solenoid rightSolenoid;
 	Solenoid leftSolenoid;
-	Timer extendedTimer;
-	Timer retractedTimer;
+	Timer extendedTimer; // A timer for measuring the time that the Tipper has been extended.
+	Timer retractedTimer; // A timer for measuring the time that the Tipper has been retracted.
 	
 	public Tipper(int rightSolenoidChannel, int leftSolenoidChannel) {
 		rightSolenoid = new Solenoid(rightSolenoidChannel);
@@ -17,6 +17,9 @@ public class Tipper {
 		
 		extendedTimer.start();
 		retractedTimer.start();
+		
+		// Tipper is initialized as retracted
+				this.retract();	//rightSolenoid is initialized as true, leftSolenoid is initialized as false.
 	}
 	
 public boolean isExtended() {
@@ -24,39 +27,39 @@ public boolean isExtended() {
 		return rightSolenoid.get(); // true = extended, false = retracted. This code works because the values must be opposite of each other and only come in 2 combinations.
 	}
 	
-	public void extend() {
+	public void extend() { // Sets the solenoid states to extend the Tipper.
 		
 		rightSolenoid.set(true);
 		leftSolenoid.set(false);
 	}
 	
-	public void retract() {
+	public void retract() { // Sets the solenoid states to retract the Tipper.
 		
 		rightSolenoid.set(false);
 		leftSolenoid.set(true);	
 	}
 	
-	public void flip() { //Switches state between extend and retract
+	public void flip() { // Flips the solenoid states of the Tipper. Example: goes from retracted to extended (or) extended to retracted.
 		
 		rightSolenoid.set(!rightSolenoid.get());
 		leftSolenoid.set(!leftSolenoid.get());	
 	}
 	
-	public double timeExtended() {
-		if (this.isExtended() == true) {
-			return extendedTimer.get();
+	public double timeExtended() { // Returns the time the Tipper has been extended.
+		if (this.isExtended() == true) { // If Tipper is extended.
+			return extendedTimer.get(); // Returns the time the Tipper has been extended.
 		} else {
-			extendedTimer.reset();
-			return 0;
+			extendedTimer.reset(); // Constantly resets the extendedTimer to zero each cycle of code when not extended. This is so the timer counts up from zero once it becomes extended.
+			return 0; // If not extended it returns a time of zero.
 		}
 	}
 	
 	public double timeRetracted() {
-		if (this.isExtended() == false) { // is Retracted.
+		if (this.isExtended() == false) { // If Tipper is retracted.
 			return retractedTimer.get();
 		} else {
-			retractedTimer.reset();
-			return 0;
+			retractedTimer.reset(); // Constantly resets the retractedTimer to zero each cycle of code when not retracted. This is so the timer counts up from zero once it becomes retracted.
+			return 0; //If not retracted it returns a time of zero.
 		}
 	}
 	
