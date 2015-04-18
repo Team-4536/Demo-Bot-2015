3a73d350f7c46ab0772ba4ec766ed6a917e2abed
MinuteBots <stepan.v.subbotin@gmail.com>
2015-04-18 13:10:01 -0500

    Deleted burgler deployment speeds

--- src/org/usfirst/frc/team4536/robot/Constants.java
+++ src/org/usfirst/frc/team4536/robot/Constants.java
@@ -71,9 +71,6 @@ public class Constants {
 	public static final double TOWER_CAN_HOLDING = 0.2;
 	public static final double TOWER_SETTING_DOWN = -0.1;
 	
-	// Can Burgler Constants
-	public static final double DEPLOYMENT_SPEED = 1.0;
-	public static final double RETRACTING_SPEED = 1.0;
 	// Normal Speed Limit
 	public static final double FORWARD_SPEED_LIMIT = 1;
 	public static final double TURN_SPEED_LIMIT = 1;
